263750feda8598d9ae98ca3d22661953c3a90955
Harry Oslislo <oslislo1@hotmail.com.com>
2017-06-16 13:05:15 -0400

    triying to upload a change

--- hello.py
+++ hello.py
@@ -24,7 +24,7 @@ def main():
     print repeat('Woo Hoo', True)  ## Woo HooWoo HooWoo Hoo!!!
     print "done"
 
-    #added a change ###try  toupload to git
+    #added a change ###try  toupload to git #no good try again
 
     f = open("testio.txt",'r')
     for line in f:
